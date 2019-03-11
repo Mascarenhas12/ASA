4ab9d6593c5495fae89329430a8ee4681ba62f0e
Manuel Mascarenhas <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2019-03-11 11:18:29 +0000

    first

---
+++ C++/Network.h
@@ -0,0 +1,11 @@
+#include <list>
+class Network{
+private:
+  int numNodes;
+public:
+  Network(int n);
+}
+
+class Sub-Net{
+
+}
---
+++ C++/hello.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  char a;
+  scanf("%c\n", &a);
+  printf("%c\n", a);
+  return 0;
+}
