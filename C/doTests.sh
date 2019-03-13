400ce7904b249400178b8c84eeb1d9b23402f2f5
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2019-03-13 02:08:08 +0000

    Added script for automatically running inputs:  - Creates a .out.mine file with user output  - Compares .out.mine with correct .out  - Warns the user and creates a .out.diff if outputs differ  - Five tests were added, ex4 and ex5 are custom made

---
+++ C/doTests.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ================================================
+# IST LEIC-T Analise e Sintese de Algoritmos 18/19
+# Project 1 - doTests.sh
+# 
+# Authors:
+# Manuel Mascarenhas - 90751
+# Miguel Levezinho   - 90756
+# ================================================
+
+# Executable should be named proj1
+# Use chmod +x doTests.sh if permission is denied
+
+i=0
+
+for test in tests/*.in; do
+	./proj1 < $test > ${test%.in}.out.mine
+
+	diff ${test%.in}.out ${test%.in}.out.mine > ${test%.in}.out.diff
+	if [ -s ${test%.in}.out.diff ]; then
+		echo " * FAILED ${test##*/}"
+		i=$[i+1]
+	else
+		rm -f ${test%.in}.out.diff ${test%.in}.out.mine
+	fi
+done
+
+if [[ $i == 0 ]]; then
+	echo "Finished."
+else
+	echo "Finished with $i failure(s)."
+fi
