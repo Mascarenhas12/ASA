3df1797895424cad372a7fef46509e896b21e2d5
Manuel Mascarenhas <manuel.d.mascarenhas@tecnico.ulisboa.pt>
2019-03-17 23:55:20 +0000

    inicio de relatório

---
+++ C/relat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ================================================
+# IST LEIC-T Analise e Sintese de Algoritmos 18/19
+# Project 1 - relat.sh
+#
+# Authors:
+# Manuel Mascarenhas - 90751
+# Miguel Levezinho   - 90756
+# ================================================
+
+# Executable should be named proj1
+# Use chmod +x relat.sh if permission is denied
+
+for i in {0..999};
+  do  time ./proj1 < tests/T06.in
